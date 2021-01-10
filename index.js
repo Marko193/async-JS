7857026217420ef548df63dee78d30d09d93f526
Marko Pavlenko <marko.pavlenko@nure.ua>
2021-01-10 16:39:27 +0200

    Do the read and writeFile correctly, with promises

--- index.js
+++ index.js
@@ -4,23 +4,48 @@
 //should always use ASYNC CODE with PROMISES instead of this
 
 const fs = require('fs');
-const superAgent = require('superagent');
+const superagent = require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data)=>{
-    console.log(`Breed: ${data}`);
+//Promise constructor to find file - promise f() with async work
+//Takes in Executor f() - called immediately, when promise is created 
+const readFilePro = file => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, (err, data) => {
+            if (err){
+                reject("File can`t be find!");
+            } 
+            resolve(data);
+    
+        });
+    });
+}
 
-    //pending promise in the begining - to get the data
-    superAgent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+//How to write to file using writeFilePro
+const writeFilePro = (file, data) => {
+    return new Promise((resolve, reject) => { 
+        fs.writeFile(file, data, err => {
+            if (err){
+                reject("Couldn`t write to file!");
+            } 
+            resolve("Succefully written!");
+        });
+    });
+};
+
+//the implement of read file with promise constructor
+//without callback hell
+readFilePro(`${__dirname}/dog.txt`)
+    .then(data =>{
+        console.log(`Breed: ${data}`);
+        //pending promise in the begining - to get the data
+        return superagent
+            .get(`https://dog.ceo/api/breed/${data}/images/random`);
+    })    
     .then(res => {
         console.log(res.body.message);
-
-        fs.writeFile('dog-image.txt', res.body.message, err =>{
-            console.log('Random dog image saved to the file!');
-        });
-        //reject(error) promise - if we get an error
-    })
+        return writeFilePro('dog-image.txt', res.body.message)})
+    .then(() =>{
+        console.log('Random dog image saved to the file!')})
     .catch(err => {
         console.log(err.message);
     });
-});
